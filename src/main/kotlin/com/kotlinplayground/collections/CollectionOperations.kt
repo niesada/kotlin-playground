c470f056d3dcd0462f0da8537ea249c64928ee8f
Eddie.Nariyoshi <Eddie.Nariyoshi@dendreon.com>
2023-11-14 08:53:47 -0800

    wip

--- src/main/kotlin/com/kotlinplayground/collections/CollectionOperations.kt
+++ src/main/kotlin/com/kotlinplayground/collections/CollectionOperations.kt
@@ -2,6 +2,7 @@ package com.kotlinplayground.collections
 
 import com.kotlinplayground.dataset.Course
 import com.kotlinplayground.dataset.CourseCategory
+import com.kotlinplayground.dataset.KAFKA
 import com.kotlinplayground.dataset.courseList
 
 fun main() {
@@ -10,15 +11,91 @@ fun main() {
     val devPredicate = { c: Course -> c.category == CourseCategory.DEVELOPEMENT }
     val desPredicate = { c: Course -> c.category == CourseCategory.DESIGN }
 
-    exploreFilter(courseList, desPredicate)
+   /* val list = listOf(listOf(1, 2, 3), listOf(4, 5, 6))
+
+    val mapResult = list.map { outerList ->
+        outerList.map {
+            it.toDouble()
+        }
+    }
+    println("mapResult : $mapResult")
+
+    val flatMapResult = list.flatMap { outerList ->
+        outerList.map {
+            it.toDouble()
+        }
+    }
+    println("flatMapResult : $flatMapResult")
+
+    //exploreFilter(courseList, desPredicate)
+
+    //exploreMap(courseList, devPredicate)
+    val courses = exploreFlatMap(courseList, KAFKA)
+    println("courses : $courses")*/
+
+    exploreHashMap()
 }
 
-fun exploreFilter(courseList: MutableList<Course>,
-                  predicate : (Course)->Boolean) {
+fun exploreHashMap() {
+
+    val nameAgeMutableMap = mutableMapOf("Dilip" to 33, "Scooby" to 5)
+    nameAgeMutableMap
+        .forEach { (k,v) ->
+            println("Key : $k and the value is $v")
+        }
+
+    val value = nameAgeMutableMap.getOrElse("Dilip1") {"abc"}
+    println("Value is $value")
+
+    val result = nameAgeMutableMap.containsKey("Dilip")
+    println("result is $result")
+
+    val filteredMap = nameAgeMutableMap.filterKeys { it.length > 5}
+        .map { it.key.uppercase() }
+    println("filteredMap is $filteredMap")
+
+    val maxAge = nameAgeMutableMap
+        .maxByOrNull { it.value }
+
+    println("maxAge is $maxAge")
+
+
+}
+
+fun exploreFlatMap(courseList: MutableList<Course>, kafka: String): List<String> {
+
+    val kafkaCourses = courseList.flatMap { course ->
+        val courseName = course.name
+        course.topicsCovered.filter {
+            it == kafka
+        }.map {
+            courseName
+        }
+    }
+    return kafkaCourses
+
+}
+
+fun exploreMap(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
+
+    val courses = courseList
+        .filter(predicate)
+        .map { "${it.name} - ${it.category}" }
+        .forEach {
+            println(it)
+        }
+    println("courses : $courses")
+
+}
+
+fun exploreFilter(
+    courseList: MutableList<Course>,
+    predicate: (Course) -> Boolean
+) {
 
     courseList
         //.filter { it.category == CourseCategory.DEVELOPEMENT }
-        .filter { predicate.invoke(it)}
+        .filter { predicate.invoke(it) }
         .forEach {
             println("Courses : $it")
         }
---
+++ src/main/kotlin/com/kotlinplayground/collections/CollectionOperationsUsingSequences.kt
@@ -0,0 +1,47 @@
+package com.kotlinplayground.collections
+
+import com.kotlinplayground.dataset.Course
+import com.kotlinplayground.dataset.CourseCategory
+import com.kotlinplayground.dataset.courseList
+
+fun main() {
+
+    val namesListUsingSequence = listOf("alex", "ben", "chloe")
+        .asSequence()
+        .filter { it.length >=4 }
+        .map { it.uppercase() }
+        .toList()
+
+    println("namesListUsingSequence : $namesListUsingSequence")
+    val devPredicate = { c: Course -> c.category == CourseCategory.DEVELOPEMENT }
+
+    exploreFilterUsingSequence(courseList(),devPredicate )
+
+    val range = 1..1000_000_000
+
+    range
+        //.asSequence()
+        .map { it.toDouble() }
+        .take(40)
+        .forEach {
+            println("Value is : $it")
+        }
+
+
+}
+
+fun exploreFilterUsingSequence(
+    courseList: MutableList<Course>,
+    predicate: (Course) -> Boolean
+) {
+
+    courseList
+        //.filter { it.category == CourseCategory.DEVELOPEMENT }
+        .asSequence()
+        .filter { predicate.invoke(it) }
+        .forEach {
+            println("Courses : $it")
+        }
+
+
+}
