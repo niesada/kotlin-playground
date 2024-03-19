c050f683ff941e1755a0b7121eb2cfc65e963f30
Eddie.Nariyoshi <Eddie.Nariyoshi@dendreon.com>
2024-03-18 17:11:22 -0700

    save update

--- src/main/kotlin/leetcode/DivideWithoutDivide.kt
+++ src/main/kotlin/leetcode/DivideWithoutDivide.kt
@@ -1,14 +1,16 @@
 package leetcode
 
-fun divideNoDivide(dividend: Int, divisor: Int): Int {
+import kotlin.math.abs
+
+fun divide(dividend: Int, divisor: Int): Int {
     if (dividend == Int.MIN_VALUE && divisor == -1) {
         // 处理溢出情况
         return Int.MAX_VALUE
     }
 
     val isNegative = (dividend < 0) xor (divisor < 0)
-    var dvd = Math.abs(dividend.toLong())
-    val dvs = Math.abs(divisor.toLong())
+    var dvd = abs(dividend.toLong())
+    val dvs = abs(divisor.toLong())
     var quotient = 0L
 
     for (i in 31 downTo 0) {
@@ -24,5 +26,5 @@ fun divideNoDivide(dividend: Int, divisor: Int): Int {
 fun main() {
     val dividend = 10
     val divisor = 3
-    println("Result of $dividend divided by $divisor is: ${divideNoDivide(dividend, divisor)}")
+    println("Result of $dividend divided by $divisor is: ${divide(dividend, divisor)}")
 }
